0064937d23d3fb91428295f19f09ca147c2ff836
Patty11 <gonzalezpatty11@gmail.com>
2024-05-16 08:56:00 -0500

    Cambio conflicto

--- src/PartsUnlimited.Models/CartItem.cs
+++ src/PartsUnlimited.Models/CartItem.cs
@@ -17,6 +17,7 @@ namespace PartsUnlimited.Models
         public string CartId { get; set; }
         public int ProductId { get; set; }
         public int Count { get; set; }
+        public integer1
 
         [DataType(DataType.DateTime)]
         public DateTime DateCreated { get; set; }
